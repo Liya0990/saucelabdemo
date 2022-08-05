23b402fe176eafd85fc81e18219e761338e35005
Nigare Dilshat <98499491+Liya0990@users.noreply.github.com>
2022-08-05 10:56:06 -0400

    latest

---
+++ src/test/java/utils/Automation.java
@@ -0,0 +1,114 @@
+package utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Automation {
+
+    private static WebDriver driver;
+    private static WebDriverWait waits;
+
+    public static void init(WebDriver inputDriver){
+        driver=inputDriver;
+        waits=new WebDriverWait(inputDriver, Duration.ofSeconds(15));
+
+    }
+    public static void visit(String url){
+        driver.get(url);
+    }
+
+    public static void click(By location ){
+        WebElement element=
+                waits.until(ExpectedConditions.visibilityOfElementLocated(location));
+        element.click();
+    }
+
+    public static void type(By location, String text){
+        WebElement element =
+                waits.until(ExpectedConditions.visibilityOfElementLocated(location));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public static String getText(By location){
+        WebElement element=
+                waits.until(ExpectedConditions.visibilityOfElementLocated(location));
+        return element.getText();
+    }
+
+    public static boolean isVisible(By location){
+        WebElement element=
+                waits.until(ExpectedConditions.visibilityOfElementLocated(location));
+        return element.isDisplayed();
+    }
+
+    public static void highlight(By location){
+        WebElement element=
+                waits.until(ExpectedConditions.visibilityOfElementLocated(location));
+        String jsCode="argument[0].setAttributes('style', 'border: 3px solid red)";
+        JavascriptExecutor jsEngine= (JavascriptExecutor) driver;
+        jsEngine.executeScript(jsCode,element);
+    }
+
+    public static String getAttribute(By location){
+        WebElement element=
+                waits.until(ExpectedConditions.visibilityOfElementLocated(location));
+        String placeholderAttribute =element.getAttribute("placeholder");
+        return  placeholderAttribute;
+
+    }
+
+    public static boolean isStringUpperCase(String str){
+
+        //convert String to char array
+        char[] charArray = str.toCharArray();
+
+        for(int i=0; i < charArray.length; i++){
+
+            //if any character is not in upper case, return false
+            if( !Character.isUpperCase( charArray[i] ))
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void selectDropDown(By location, String optionText ){
+        WebElement element=
+                waits.until(ExpectedConditions.visibilityOfElementLocated(location));
+        Select dropDownSelection =new Select(element);
+        dropDownSelection.selectByVisibleText(optionText);
+
+    }
+    public static String getFirstSelectedOption(By location){
+        WebElement element=
+                waits.until(ExpectedConditions.visibilityOfElementLocated(location));
+        Select dropDownSelection =new Select(element);
+       return dropDownSelection.getFirstSelectedOption().getText();
+
+
+
+    }
+
+
+
+    public static void sleep(int sec){
+        try {
+           Thread.sleep(sec*1000);
+        }catch (InterruptedException ignore){
+            // do nothing
+        }
+    }
+
+
+
+
+
+}
---
+++ src/test/java/utils/TestDetector.java
@@ -0,0 +1,73 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestDetector implements ITestListener {
+
+    private String path=System.getProperty("user.dir")+"/reports/";
+    private ExtentReports reportManager ;
+    private ExtentSparkReporter spark;
+    private static ExtentTest testCaseSection;
+
+
+    // each test cycle related
+    @Override
+    public void onStart(ITestContext testCycle){
+        reportManager =new ExtentReports();
+        spark=new ExtentSparkReporter(path);
+        spark.config().setDocumentTitle("Automated Test Report");
+        spark.config().setTheme(Theme.STANDARD);
+        spark.config().thumbnailForBase64(true);
+        reportManager.attachReporter(spark);
+    }
+
+    @Override
+    public void onFinish(ITestContext testCycle){
+        reportManager.flush();
+
+    }
+
+    // each test case related
+    @Override
+    public void onTestStart(ITestResult testCase){
+        String testCaseName=testCase.getName();
+        testCaseSection=reportManager.createTest(testCaseName);
+
+
+    }
+    /**
+     * use this mehtod to get access to test case sectons in the report for the current test case that is being executed
+     */
+    public static ExtentTest getCurrentTestCaseSection(){
+        return testCaseSection;
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult testCase){
+        testCaseSection.pass("Test case passed");
+//        String screenshotPic=DriverUtil.takeScreenshot();
+//        testCaseSection.addScreenCaptureFromBase64String(screenshotPic);
+    }
+
+    @Override
+    public void onTestFailure(ITestResult testCase){
+        testCaseSection.fail("Test failed");
+        testCaseSection.fail(testCase.getThrowable());
+//        String screenshotPic=DriverUtil.takeScreenshot();
+//        testCaseSection.addScreenCaptureFromBase64String(screenshotPic);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult testCase){
+        testCaseSection.skip("test case has been blocked");
+    }
+
+
+}//end :: class
+
